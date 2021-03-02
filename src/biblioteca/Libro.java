c2306f35c58990ad8e10cbec7c84e0301deabac1
Marcus <marcusskellyburgos@gmail.com>
2021-03-02 14:23:39 +0100

    Third commit Creacion de paquetes mas clase libro

---
+++ src/biblioteca/Libro.java
@@ -0,0 +1,12 @@
+package biblioteca;
+
+/**
+ * La clase <code>Libro</code> alberga los atributos de Libro del paquete biblioteca.
+ * de ella pueden heredar otras clases
+ * @author Marcus Skelly
+ * @version 1.0
+ * @since 1.0
+ */
+public class Libro {
+
+}
